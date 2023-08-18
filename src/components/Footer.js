8730ef78ffb07960a88ecd571cf33c1bb4c27528
ria0-o <138983380+ria0-o@users.noreply.github.com>
2023-08-18 19:54:34 +0800

    change footer color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-100 py-4 bg-black text-white text-center'>
+    <div className='w-100 py-4 bg-white text-black text-center'>
       <img src='/logo.png' alt='upang' width={60}/>
 
       <h2 className='fw-bold'>PHINMA University of Pangasinan</h2>
